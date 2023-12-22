6c94726bb353db2ba27a6c07400210b9ad770151
Dechamps Chen <dechamps.chen@gmail.com>
2023-12-22 14:21:19 +0100

    Création de la formulaire ajout

---
+++ src/Form/Type/WineType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class WineType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => 'Nom du vin : '])
+            ->add('description', TextType::class, ['label' => 'Description : '])
+            ->add('region', ChoiceType::class)
+            ->add('add', SubmitType::class);
+    }
+}
