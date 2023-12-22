e34d68f80269067a0792aed703dd37db32f2b072
Dechamps Chen <dechamps.chen@gmail.com>
2023-12-22 14:20:25 +0100

    Ajout de la page d'ajout / méthode add() dans WineController.php

--- src/Controller/WineController.php
+++ src/Controller/WineController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Wine;
+// use App\Entity\Region;
+use App\Form\Type\WineType;
 use App\Repository\WineRepository;
+use App\Repository\RegionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class WineController extends AbstractController
@@ -17,6 +23,44 @@ class WineController extends AbstractController
 
         return $this->render('wine/index.html.twig', [
             'controller_name' => 'WineController', 'wines' => $wines,
+        ]);
+    }
+
+    #[Route('/add', name: 'app_wine_add')]
+    public function add(Request $request, RegionRepository $regionRepo, EntityManagerInterface $entityManager): Response
+    {
+        $wine = new Wine();
+        $regions = $regionRepo->findBy([], ['numero' => 'ASC']);
+
+        $regionsOrdered = [];
+        foreach ($regions as $region) {
+            $regionsOrdered[$region->getName()] = $region; // Use the Region entity as the value
+        }
+
+        $form = $this->createForm(WineType::class, $wine)
+            ->add('region', ChoiceType::class, [
+                'label' => 'Region :',
+                'choices' => $regionsOrdered,
+                'choice_label' => 'name', // Assuming Region entity has a "name" property
+            ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $wine = $form->getData();
+
+            // At this point, $wine->getRegion() will return the selected Region entity
+
+            $entityManager->persist($wine);
+            $entityManager->flush();
+
+            // Redirect to the desired route after successful form submission
+            return $this->redirectToRoute('app_wine');
+        }
+
+        return $this->render('wine/add.html.twig', [
+            'controller_name' => 'ArticleController',
+            'form' => $form->createView(),
         ]);
     }
 }
